52249083e14f12b62cdae94deedd7562844bd641
seeseekey <kontakt@seeseekey.net>
2015-07-18 21:38:48 +0200

    Set up test library for Xevle.Math library.

---
+++ Tests.Xevle.Math/Tuples/Tuple3dcTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using Xevle.Math.Tuples;
+
+namespace Tests.Xevle.Math
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestConstructor ()
+		{
+			Tuple3dc tuple = new Tuple3dc (1, 2, 3);
+			Assert.AreEqual (1, tuple.x);
+			Assert.AreEqual (2, tuple.y);
+			Assert.AreEqual (3, tuple.z);
+		}
+	}
+}
+
